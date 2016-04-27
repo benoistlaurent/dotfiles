32b199ad6654b8cf681b51ab756991118776fea5
Benoist LAURENT <benoist.laurent@gmail.com>
2016-04-27 16:24:00 +0200

    added the boostrap script

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+function doIt() {
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+          -avh --no-perms $* . ~;
+    ! [[ $* =~ 'dry-run' ]] && source ~/.bash_profile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    doIt;
+else
+    echo "Those files/directories will be copied into you home directory:";
+    echo "";
+    doIt --dry-run;
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
+fi;
+unset doIt;
